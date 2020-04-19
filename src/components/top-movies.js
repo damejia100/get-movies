c9537bd331b72505cf3e17d7314aa598ec26bccc
devAM100 <devismejia@gmail.com>
2020-04-19 19:27:23 -0400

    testing hidden api key

--- src/components/top-movies.js
+++ src/components/top-movies.js
@@ -1,11 +1,44 @@
 import React from 'react'
+import Axios from 'axios'
 
-const TopMovies = () => {
-  return (
-    <div>
-      <h2>Top Movies</h2>
-    </div>
-  )
+class TopMovies extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topTwenty: []
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const {data} = await Axios.get('https://api.themoviedb.org/3/movie/popular?api_key=1234')
+      this.setState({
+        topTwenty: data.results
+      })
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+  render() {
+
+    return (
+      <div>
+        <h2>Top Movies</h2>
+        <div className='top-movies-list'>
+          {this.state.topTwenty.map(movie => {
+            return (
+              <div key={movie.id}>{movie.title}</div>
+            )
+          })}
+        </div>
+
+      </div>
+    )
+
+  }
+
 }
+
 
 export default TopMovies
